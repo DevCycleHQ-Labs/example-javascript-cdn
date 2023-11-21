eab402f5eefc222d71181a0ac17a6b69a019e95d
Emma Willis <ehenriks@uwo.ca>
2023-11-21 12:16:26 -0500

    Update js cdn example to match spec

--- scripts/devcycle.js
+++ scripts/devcycle.js
@@ -1,56 +1,77 @@
-if (typeof DEVCYCLE_CLIENT_SDK_KEY === 'undefined') {
-    alert("Please create a .env file and set the DEVCYCLE_CLIENT_SDK_KEY environment variable.");
-} 
+const users = [
+    {
+        user_id: 'user-1',
+        name: 'User 1',
+        email: 'user1@email.com'
+    },
+    {
+        user_id: 'user-2',
+        name: 'User 2',
+        email: 'user2@email.com'
+    },
+    {
+        user_id: 'user-3',
+        name: 'User 3',
+        email: 'user3@email.com'
+    }
+]
 
-const user = {
-    // Edit the user properties here to see how they interact with your feature targeting rules
-    user_id: "user1",
-    email: "firstuser@email.com",
-};
+// Create DevCycle client and set up event listeners
+const setUpDevCycle = () => {
+    if (typeof DEVCYCLE_CLIENT_SDK_KEY === 'undefined') {
+        alert('Set your DEVCYCLE_CLIENT_SDK_KEY environment variable to use the DevCycle JavaScript SDK.')
+    }
 
-const newUser = {
-    // Try editing your feature's targeting rules in the Development environment to serve Variation Off to this user 
-    // then click the "Identify New User" button to see the new user's experience
-    // Note: targeting rules are evaluated from top to bottom, so the first rule that matches will be used
-    user_id: "user2",
-    email: "newuser@email.com",
-};
+    // Initialize the DevCycle client with your SDK key and user
+    const devcycleOptions = { logLevel: "debug" };
+    const devcycleClient = DevCycle.initializeDevCycle(
+        DEVCYCLE_CLIENT_SDK_KEY,
+        users[0], // identifying initial user as user-1
+        devcycleOptions
+    );
 
-// Initialize the DevCycle client with your SDK key and user
-const devcycleOptions = { logLevel: "debug" };
-const devcycleClient = DevCycle.initializeDevCycle(
-    DEVCYCLE_CLIENT_SDK_KEY,
-    user,
-    devcycleOptions
-);
+    setUpIdentifyDropdown(devcycleClient);
+    document.getElementById("reset").onclick = () => {
+        devcycleClient.resetUser().then(() => updateUI(devcycleClient));
+        document.getElementById("identify").options[3].selected = true;
+    };
 
+    // Update the app when DevCycle receives the first user config
+    devcycleClient.onClientInitialized().then(({ config }) => {
+        updateUI(devcycleClient);
+    });
 
-devcycleClient.onClientInitialized().then(({ config }) => {
-    console.log("DevCycle client initialized with configuration: ", config);
-});
+    // Update the app when DevCycle receives realtime updates from the dashboard
+    devcycleClient.subscribe('configUpdated', () => {
+        updateUI(devcycleClient);
+    });
+}
 
-document.getElementById("identify").onclick = function () {
-    devcycleClient.identifyUser(newUser).then(updateUI);
-};
+const setUpIdentifyDropdown = (devcycleClient) => {
+    const identifyDropdown = document.getElementById("identify");
 
-document.getElementById("reset").onclick = function () {
-    devcycleClient.resetUser().then(updateUI);
-};
+    identifyDropdown.innerHTML = `
+        ${users.map(user => (
+         `<option key='${user.user_id}' value='${JSON.stringify(user)}'>${user.name}</option>`
+        ))}
+        <option key="anonymous" value='{}'>Anonymous User</option>
+    `
 
+    identifyDropdown.onchange = (event) => {
+        devcycleClient.identifyUser(JSON.parse(event.target.value)).then(() => updateUI(devcycleClient));
+    }
+}
 
 // Use DevCycle variables to control the UI 
-const updateUI = () => {
-    document.getElementById("user").innerHTML = `Current User: ${devcycleClient.user?.email || "Anonymous User"}`;
+const updateUI = (devcycleClient) => {
+    const greeting = devcycleClient.variableValue('togglebot-greeting', 'Hello world!')
+    const shouldWink = devcycleClient.variableValue('togglebot-wink', false)
+    const spinSpeed = devcycleClient.variableValue('togglebot-speed', 'off')
+    
+    document.getElementById("greeting").innerHTML = greeting;
+    const togglebot = document.getElementById("togglebot");
+    togglebot.src = shouldWink ? "./images//togglebot-wink.png" : "./images/togglebot.png";
 
-    const value = devcycleClient.variableValue("greeting", "default");
-    document.getElementById("greeting").innerHTML = value;
-
-    const shouldSpin = devcycleClient.variableValue("spin-logo", false);
-    document.getElementById("logo").className = shouldSpin ? "spin" : "";
+    togglebot.classList.remove("spin-slow", "spin-fast", "spin-super-fast", "spin-off");
+    togglebot.classList.add(`spin-${spinSpeed}`);
 };
-
-
-// Update the app when DevCycle receives updates from the dashboard
-devcycleClient.subscribe('configUpdated', () => {
-    updateUI();
-});
